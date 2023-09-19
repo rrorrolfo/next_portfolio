fc5b1254d315b17944595c67ff98652ee615bb50
Rodolfo Perez <rodolfobeste@gmail.com>
2023-09-19 18:14:42 +0300

    Created SmallR logo component

--- components/logos/index.tsx
+++ components/logos/index.tsx
@@ -81,4 +81,82 @@ const RLogo = () => {
   );
 };
 
+export const SmallR = () => {
+  return (
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width="100px"
+      height="100px"
+      viewBox="0 0 1200 1200"
+      enableBackground="new 0 0 1200 1200"
+      xmlSpace="preserve"
+    >
+      <g>
+        <g>
+          <g>
+            <linearGradient
+              id="SVGID_1_"
+              gradientUnits="userSpaceOnUse"
+              x1="524.5398"
+              y1="1265.8043"
+              x2="682.724"
+              y2="734.7591"
+              gradientTransform="matrix(1 0 0 -1 294 1405)"
+            >
+              <stop offset="0" style={{ stopColor: "rgb(231, 216, 14)" }} />
+              <stop
+                offset="0.0603"
+                style={{ stopColor: "rgb(234, 220, 18)" }}
+              />
+              <stop offset="0.1527" style={{ stopColor: "rgb(237,224,22)" }} />
+              <stop offset="0.265" style={{ stopColor: "rgb(240,229,26)" }} />
+              <stop offset="0.2943" style={{ stopColor: "rgb(243,235,30)" }} />
+              <stop offset="0.3767" style={{ stopColor: "rgb(246,239,34)" }} />
+              <stop offset="0.4811" style={{ stopColor: "rgb(249,243,38)" }} />
+              <stop offset="0.5973" style={{ stopColor: "rgb(252,246,42)" }} />
+              <stop offset="0.7212" style={{ stopColor: "rgb(255,249,46)" }} />
+              <stop offset="0.8277" style={{ stopColor: "rgb(255,251,50)" }} />
+            </linearGradient>
+            <path
+              fill="#fffb32"
+              d="M767.931,56.517l-150.853,0.295l-23.05,136.792h158.888c0,0,171.539-9.005,151.438,78.094
+          c0,0,3.065,70.427-147.399,67.001c-6.307,37.82-28.141,135.341-28.141,135.341s328.226,27.45,350.66-186.048
+          C1093.295,156.459,1047.061,53.309,767.931,56.517z"
+            />
+          </g>
+
+          <linearGradient
+            id="SVGID_3_"
+            gradientUnits="userSpaceOnUse"
+            x1="580.5635"
+            y1="952.2674"
+            x2="746.2073"
+            y2="396.1798"
+            gradientTransform="matrix(1 0 0 -1 294 1405)"
+          >
+            <stop offset="0" style={{ stopColor: "rgb(231, 216, 14)" }} />
+            <stop offset="0.0603" style={{ stopColor: "rgb(234, 220, 18)" }} />
+            <stop offset="0.1527" style={{ stopColor: "rgb(237,224,22)" }} />
+            <stop offset="0.265" style={{ stopColor: "rgb(240,229,26)" }} />
+            <stop offset="0.2943" style={{ stopColor: "rgb(243,235,30)" }} />
+            <stop offset="0.3767" style={{ stopColor: "rgb(246,239,34)" }} />
+            <stop offset="0.4811" style={{ stopColor: "rgb(249,243,38)" }} />
+            <stop offset="0.5973" style={{ stopColor: "rgb(252,246,42)" }} />
+            <stop offset="0.7212" style={{ stopColor: "rgb(255,249,46)" }} />
+            <stop offset="0.8277" style={{ stopColor: "rgb(255,251,50)" }} />
+          </linearGradient>
+          <path
+            fill="rgb(231, 216, 14)"
+            d="M1133.495,725.956c-56.134-57.584-377.829-386.913-377.829-386.913l-38.754,180.476
+        l190.123,206.437H1133.495z"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+};
+
 export default RLogo;
