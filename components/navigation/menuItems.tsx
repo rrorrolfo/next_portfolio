a41b4993e7e3d894c5e2c08976aa98266e0f273e
Rodolfo Perez <rodolfobeste@gmail.com>
2023-09-19 18:18:00 +0300

    Created pageItems data

---
+++ components/navigation/menuItems.tsx
@@ -0,0 +1,22 @@
+import { LinkItem } from "./types";
+
+export const pagesItems: LinkItem[] = [
+  {
+    id: "work",
+    text: "Work",
+    to: "/work",
+    title: "Work",
+  },
+  {
+    id: "about",
+    text: "About",
+    to: "/about",
+    title: "About",
+  },
+  {
+    id: "contact",
+    text: "Contact",
+    to: "/contact",
+    title: "Contact",
+  },
+];
