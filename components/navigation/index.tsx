329103283c8e605480555be4b6a0044a99ea61ee
Rodolfo Perez <rodolfobeste@gmail.com>
2023-09-20 16:18:30 +0300

    Created and styled navigation menu for navigation links

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -3,6 +3,7 @@ import Grid from "@mui/material/Unstable_Grid2";
 
 import DropdownMenu from "./components";
 import styles from "./navigation.module.css";
+import { pagesItems } from "./menuItems";
 
 import { SmallR } from "../logos";
 
@@ -11,10 +12,11 @@ const Navigation = () => {
     <Grid
       container
       xs={11}
+      md={12}
       component="nav"
       className={styles.navigationContainer}
     >
-      <Grid xs={8} sm={9} className={styles.logoContaine}>
+      <Grid xs={8} sm={9} md={3} lg={2} className={styles.logoContaine}>
         <a
           href="/"
           title="Home"
@@ -25,15 +27,21 @@ const Navigation = () => {
           <SmallR />
         </a>
       </Grid>
-      {/* <Grid md={6} lg={6} className={styles.mainNavContainer}>
+      <Grid md={6} lg={4} className={styles.mainNavContainer}>
         <Grid container>
-          <Grid>
-            <a href="/work" title="Work" className="mainNavLink">
-              work
-            </a>
-          </Grid>
+          {pagesItems.map(({ id, text, to, title }) => (
+            <Grid
+              key={id}
+              md={12 / pagesItems.length}
+              className={styles.mainNavLink}
+            >
+              <a href={to} title={title}>
+                {text}
+              </a>
+            </Grid>
+          ))}
         </Grid>
-      </Grid> */}
+      </Grid>
       <Grid xs={2} sm={1} className={styles.menu}>
         <DropdownMenu />
       </Grid>
