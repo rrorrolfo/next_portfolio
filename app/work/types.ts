467705e2795285968d03afdcd1d3efcae1dae9b8
Rodolfo Perez <rodolfobeste@gmail.com>
2023-09-28 18:22:25 +0300

    Created project type

---
+++ app/work/types.ts
@@ -0,0 +1,11 @@
+export type Project = {
+  id: string;
+  name: string;
+  description: string;
+  imageURL: string;
+  imageAlt: string;
+  repositoryURL: string;
+  liveUrl: string;
+  technologiesUsed: string[];
+  technicalSpecifications: string[];
+};
