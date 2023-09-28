8263a065f08e78f0bf6bd3f27ae7e929ebc3fcd1
Rodolfo Perez <rodolfobeste@gmail.com>
2023-09-29 00:48:29 +0300

    Updated projects cards container and project cards component breakpoint values

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -10,9 +10,9 @@ const Work = () => {
       <Grid xs={12}>
         <h1 className={styles.title}>My Work</h1>
       </Grid>
-      <Grid container spacing={4} xs={10} lg={9}>
+      <Grid container spacing={4} xs={9} sm={10} lg={9}>
         {projectsData.map((project) => (
-          <Grid xs={6} md={4} key={project.id}>
+          <Grid xs={12} sm={6} md={4} key={project.id}>
             <ProjectCard project={project} />
           </Grid>
         ))}
