2a6eb3aecff05699afaebf58867a3df5af86106c
Rodolfo Perez <rodolfobeste@gmail.com>
2023-09-28 18:22:39 +0300

    Created projects data dictionary

---
+++ app/work/projectsData.ts
@@ -0,0 +1,78 @@
+import { Project } from "./types";
+
+const projectsData: Project[] = [
+  {
+    id: "ctm",
+    name: "Creditamina",
+    description: "Lead generator",
+    imageURL:
+      "https://storage.googleapis.com/creditamina/landing/amor-y-amistad-febrero.jpg",
+    imageAlt: "Crexitamina alt",
+    repositoryURL: "https://github.com/rrorrolfo/creditamina",
+    liveUrl: "https://creditamina.mx/",
+    technologiesUsed: ["React.js", "Next.js"],
+    technicalSpecifications: ["This is alead generator", "it is very cool"],
+  },
+  {
+    id: "ctm",
+    name: "Creditamina",
+    description: "Lead generator",
+    imageURL:
+      "https://storage.googleapis.com/creditamina/landing/amor-y-amistad-febrero.jpg",
+    imageAlt: "Crexitamina alt",
+    repositoryURL: "https://github.com/rrorrolfo/creditamina",
+    liveUrl: "https://creditamina.mx/",
+    technologiesUsed: ["React.js", "Next.js"],
+    technicalSpecifications: ["This is alead generator", "it is very cool"],
+  },
+  {
+    id: "ctm",
+    name: "Creditamina",
+    description: "Lead generator",
+    imageURL:
+      "https://storage.googleapis.com/creditamina/landing/amor-y-amistad-febrero.jpg",
+    imageAlt: "Crexitamina alt",
+    repositoryURL: "https://github.com/rrorrolfo/creditamina",
+    liveUrl: "https://creditamina.mx/",
+    technologiesUsed: ["React.js", "Next.js"],
+    technicalSpecifications: ["This is alead generator", "it is very cool"],
+  },
+  {
+    id: "ctm",
+    name: "Creditamina",
+    description: "Lead generator",
+    imageURL:
+      "https://storage.googleapis.com/creditamina/landing/amor-y-amistad-febrero.jpg",
+    imageAlt: "Crexitamina alt",
+    repositoryURL: "https://github.com/rrorrolfo/creditamina",
+    liveUrl: "https://creditamina.mx/",
+    technologiesUsed: ["React.js", "Next.js"],
+    technicalSpecifications: ["This is alead generator", "it is very cool"],
+  },
+  {
+    id: "ctm",
+    name: "Creditamina",
+    description: "Lead generator",
+    imageURL:
+      "https://storage.googleapis.com/creditamina/landing/amor-y-amistad-febrero.jpg",
+    imageAlt: "Crexitamina alt",
+    repositoryURL: "https://github.com/rrorrolfo/creditamina",
+    liveUrl: "https://creditamina.mx/",
+    technologiesUsed: ["React.js", "Next.js"],
+    technicalSpecifications: ["This is alead generator", "it is very cool"],
+  },
+  {
+    id: "ctm",
+    name: "Creditamina",
+    description: "Lead generator",
+    imageURL:
+      "https://storage.googleapis.com/creditamina/landing/amor-y-amistad-febrero.jpg",
+    imageAlt: "Crexitamina alt",
+    repositoryURL: "https://github.com/rrorrolfo/creditamina",
+    liveUrl: "https://creditamina.mx/",
+    technologiesUsed: ["React.js", "Next.js"],
+    technicalSpecifications: ["This is alead generator", "it is very cool"],
+  },
+];
+
+export default projectsData;
