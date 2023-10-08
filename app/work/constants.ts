141a8c54eb02f64cb5ce6f90f87e5a2216a38e76
Rodolfo Perez <rodolfobeste@gmail.com>
2023-10-08 19:31:39 +0300

    Added technologies constants

---
+++ app/work/constants.ts
@@ -0,0 +1,5 @@
+export const materialUI = "Material UI";
+export const reactjs = "React.js";
+export const nextjs = "Next.js";
+export const reduxjs = "Redux.js";
+export const bootstrap = "React Bootstrap";
